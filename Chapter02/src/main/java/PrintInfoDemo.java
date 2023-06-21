c8e3ec0dfe7dd766bcee2d0861f62fee04276b84
sixlemons <2644731467@qq.com>
2023-06-21 22:58:51 +0800

    第二章提交

---
+++ Chapter02/src/main/java/PrintInfoDemo.java
@@ -0,0 +1,16 @@
+/**
+ * @Author: L.N
+ * @Date: 2023/6/21 22:11
+ * @Description:打印个人信息
+ */
+public class PrintInfoDemo {
+        public static void main(String[] args) {
+            System.out.println("姓名:谷姐");
+            System.out.println("学校：谷粒学院");
+            System.out.println("年龄：18");
+            System.out.println("性别：女");
+            System.out.println("身高：168");
+            System.out.println("体重：108.5");
+            System.out.println("是否已婚：否");
+        }
+    }
